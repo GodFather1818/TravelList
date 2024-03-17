5ff38826a16a888fd02ab932a794a7997bd6ce81
GodFather1818 <sahilshah0620@gmail.com>
2024-03-17 15:45:53 +0530

    Final Commit

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,6 @@ import  Stats  from "./Stats";
 import Items from "./Items";
 
 
-// const initialItems = [
-//   { id: 1, description: "Passports", quantity: 2, packed: false },
-//   { id: 2, description: "Socks", quantity: 12, packed: false },
-//   { id: 3, description: "Charger", quantity: 2, packed: true },
-// ];
-
 export default function App() {
   const [items, setItems] = useState([]);
 
